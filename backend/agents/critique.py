c43a4e2852f03449f234cc3b2cdb683849a116ff
Rotem B. Weiss <91344214+rotemweiss57@users.noreply.github.com>
2024-01-22 18:19:44 -0500

    modify critique agent

--- backend/agents/critique.py
+++ backend/agents/critique.py
@@ -9,18 +9,17 @@ class CritiqueAgent:
     def critique(self, article: dict):
         prompt = [{
             "role": "system",
-            "content": "You are a newspaper writing critique. Your sole purpose is to provide feedback on a written "
+            "content": "You are a newspaper writing critique. Your sole purpose is to provide short feedback on a written "
                        "article so the writer will know what to fix.\n "
         }, {
             "role": "user",
             "content": f"Today's date is {datetime.now().strftime('%d/%m/%Y')}\n."
                        f"{str(article)}\n"
-                       f"Your task is to provide a short feedback on the article only if necessary.\n"
+                       f"Your task is to provide a really short feedback on the article only if necessary.\n"
                        f"if you think the article is good, please return None.\n"
                        f"if you noticed the field 'message' in the article, it means the writer has revised the article"
                         f"based on your previous critique. you can provide feedback on the revised article or just "
-                       f"return None if "
-                        f"you think the article is good.\n"
+                       f"return None if you think the article is good.\n"
                         f"Please return a string of your critique or None.\n"
         }]
 
